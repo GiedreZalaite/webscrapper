66b0399a12df8c49bebe305536de84df9c0fefc8
Giedre Zalaite <71758111+GiedreZalaite@users.noreply.github.com>
2021-12-19 18:51:23 +0200

    Add files via upload

---
+++ Scrapper_devops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scrapper_devops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
